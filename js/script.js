5aac74b9eacb333ff02b5372519ef9a77978f602
DiceglieA <angelodiceglie10@gmail.com>
2022-11-02 15:28:53 +0100

    fine eserciziogrid + bonus

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 let eleGrid = document.querySelector('.grid')
 let button = document.getElementById('play')
+let difficulty = document.getElementById('difficulty')
 button.addEventListener('click', play) 
 
 function play() {
     eleGrid.innerHTML = ''
-    for ( let i = 1; i <= 100; i++) {
+    for ( let i = 1; i <= difficulty.value * difficulty.value; i++) {
     const eleCell = document.createElement('div')
     eleCell.classList.add('cell')
+    eleCell.style.width = "calc(100% / "+difficulty.value+")"
     eleCell.innerHTML = `${i}`
     eleGrid.append(eleCell)
 
