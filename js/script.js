c505e1301853ccc4ba873ded31701ecf2cbde930
DiceglieA <angelodiceglie10@gmail.com>
2022-11-02 13:59:42 +0100

    creata grid in js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 let eleGrid = document.querySelector('.grid')
 
-for (let i = 1; i >= 100; i++) {
-    
+for ( let i = 1; i <= 100; i++) {
+    const eleCell = document.createElement('div')
+    eleCell.classList.add('cell')
+    eleCell.innerHTML = `${i}`
+    eleGrid.append(eleCell)
 }
