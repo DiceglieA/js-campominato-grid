42ed990c0bcfdcc28bc42b616f5f7eca633b9107
DiceglieA <angelodiceglie10@gmail.com>
2022-11-02 15:17:45 +0100

    add function click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 let eleGrid = document.querySelector('.grid')
+let button = document.getElementById('play')
+button.addEventListener('click', play) 
 
-for ( let i = 1; i <= 100; i++) {
+function play() {
+    eleGrid.innerHTML = ''
+    for ( let i = 1; i <= 100; i++) {
     const eleCell = document.createElement('div')
     eleCell.classList.add('cell')
     eleCell.innerHTML = `${i}`
@@ -9,4 +13,5 @@ for ( let i = 1; i <= 100; i++) {
     eleCell.addEventListener('click', function () {
         this.classList.toggle('active')  
     }) 
+}
 }
